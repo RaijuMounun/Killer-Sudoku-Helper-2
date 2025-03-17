ddb122e353667d9b647447a9b156a95bb98da69e
Eren Keskinoğlu <erenkeskinoglu@outlook.com>
2025-03-18 01:23:33 +0300

    Project scaffold

---
+++ main.py
@@ -0,0 +1,13 @@
+""" This is the main file for killer sudoku helper project. """
+import tkinter as tk
+import customtkinter as ctk
+
+
+root = ctk.CTk()
+root.title("Killer Sudoku Helper")
+
+
+
+
+
+root.mainloop()
